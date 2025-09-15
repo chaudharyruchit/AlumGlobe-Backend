af29014f852c12da83d798771380fdfdce322a0a
Vanshika06052005 <vanshikashukla06@gmail.com>
2025-09-16 00:30:29 +0530

    Add files via upload

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import CustomUser, College
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'college', 'is_approved')
+    list_filter = ('role', 'college', 'is_approved')
+    search_fields = ('username', 'email')
+    actions = ['approve_users']
+
+    # Bulk approve action
+    def approve_users(self, request, queryset):
+        queryset.update(is_approved=True, is_active=True)
+    approve_users.short_description = "Approve selected users"
+
+    # College admin only sees their college users
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.role == 'admin' and request.user.college:
+            return qs.filter(college=request.user.college)
+        return qs
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'domain')
+    search_fields = ('name', 'code')
 
 # Register your models here.
